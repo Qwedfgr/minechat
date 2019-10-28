d7a33aa2f481afc0274a84646af8ebe0ee7f9b8f
Qwedfgr <georgiykvelidze@gmail.com>
2019-10-28 21:03:32 +0300

    добавил аргументы командной строки

--- main.py
+++ main.py
@@ -7,20 +7,29 @@ import dotenv
 from aiofile import AIOFile
 
 
+def get_arguments_parser():
+    formatter_class = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=formatter_class)
+    parser.add_argument('--host', type=str, default=os.getenv('HOST'), help='set host')
+    parser.add_argument('--port', type=int, default=os.getenv('PORT'), help='set port')
+    parser.add_argument('--history', type=str, default=os.getenv('HISTORY'), help='set path to history file')
+    return parser
+
+
 async def main():
     dotenv.load_dotenv()
-    host = os.getenv('HOST')
-    port = os.getenv('PORT')
-    history_file = os.getenv('HISTORY')
+
+    parser = get_arguments_parser()
+    args = parser.parse_args()
 
     while True:
         try:
-            reader, writer = await asyncio.open_connection(host=host, port=port)
+            reader, writer = await asyncio.open_connection(host=args.host, port=args.port)
             while True:
                 message_text = (await reader.readline()).decode()
                 message_date = datetime.strftime(datetime.now(), '%y.%m.%d %H:%M')
                 message = f'[{message_date}] {message_text}'
-                async with AIOFile(history_file, 'a') as my_file:
+                async with AIOFile(args.history, 'a') as my_file:
                     await my_file.write(message)
         except ConnectionRefusedError:
             print('Нет соединения. Повторная попытка')
